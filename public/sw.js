f1d388feb5d423edc8dabf316f58d73a33d59eaa
nae-imUam <nae.imuam103@gmail.com>
2026-02-08 23:03:44 +0530

    chatbox

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,10 @@
-const CACHE_NAME = 'chat-cache-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png'];
+const CACHE_NAME = 'chat-v1';
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(['/']);
+    })
   );
 });
 
