e62e06ac02a0cc57b205282253720ddc8261371b
nae-imUam <nae.imuam103@gmail.com>
2026-02-08 22:42:17 +0530

    chatbox

--- public/sw.js
+++ public/sw.js
@@ -1,19 +1,2 @@
-const CACHE_NAME = 'secure-chat-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png'
-];
-
-self.addEventListener('install', (html) => {
-  html.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
-});
+self.addEventListener('install', (e) => self.skipWaiting());
+self.addEventListener('fetch', (e) => e.respondWith(fetch(e.request)));
